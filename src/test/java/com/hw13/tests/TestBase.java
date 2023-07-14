f6712aa65f81962efe2ffad1d18732baab60e356
ishishkina <shishkinaira@gmail.com>
2023-07-15 00:45:25 +0300

    Configuration.browserSize commented

--- src/test/java/com/hw13/tests/TestBase.java
+++ src/test/java/com/hw13/tests/TestBase.java
@@ -7,7 +7,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+     //   Configuration.browserSize = "1920x1080";
     //    Configuration.holdBrowserOpen = true;
     }
 }
