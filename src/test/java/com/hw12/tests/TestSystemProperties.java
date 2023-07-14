2d944bdf93b2cb9c4d1ffc749cdd7654288e0692
ishishkina <shishkinaira@gmail.com>
2023-07-14 20:56:29 +0300

    property_test duild.gradle fix

--- src/test/java/com/hw12/tests/TestSystemProperties.java
+++ src/test/java/com/hw12/tests/TestSystemProperties.java
@@ -3,10 +3,10 @@ package com.hw12.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class Property {
+public class TestSystemProperties {
     @Test
     @Tag("properties")
-    void checkProperties() {
+    void testSystemProperties() {
         System.out.println(System.getProperty("selenoid"));
         System.out.println(System.getProperty("baseUrl"));
         System.out.println(System.getProperty("browser"));
