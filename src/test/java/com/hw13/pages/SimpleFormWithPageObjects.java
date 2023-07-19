b4bdc8e3937e10e4cf00a627455853e5de579ad8
ishishkina <shishkinaira@gmail.com>
2023-07-19 10:47:38 +0300

    Tags Steps on the Page file

--- src/test/java/com/hw13/pages/SimpleFormWithPageObjects.java
+++ src/test/java/com/hw13/pages/SimpleFormWithPageObjects.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.SelenideElement;
 import com.hw13.pages.components.CalendarComponent;
 import com.hw13.pages.components.CityComponent;
 import com.hw13.pages.components.VerifyComponent;
+import io.qameta.allure.Step;
 
 import java.io.File;
 
@@ -27,42 +28,47 @@ public class SimpleFormWithPageObjects {
             subjectsInput = $("#subjectsInput"),
             pictureFile = $("#uploadPicture"),
             submitButton = $("#submit");
-
+    @Step("Open page")
     public SimpleFormWithPageObjects openPage() {
         open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
 
         return this;
     }
+
+    @Step("Remove banner")
     public SimpleFormWithPageObjects removeBanner() {
         executeJavaScript("$('#fixedban').remove()");
         return this;
     }
+
+    @Step("Set First Name")
     public SimpleFormWithPageObjects setFirstName(String value) {
         firstNameInput.setValue(value);
         return this;
     }
 
+    @Step("Set Last Name")
     public SimpleFormWithPageObjects setLastName(String value) {
         lastNameInput.setValue(value);
         return this;
     }
-
+    @Step("Set Email")
     public SimpleFormWithPageObjects setUserEmailName(String value) {
         userEmailInput.setValue(value);
         return this;
     }
-
+    @Step("Set Gander")
     public SimpleFormWithPageObjects setGender(String value) {
         ganderWrapper.$(byText(value)).click();
         return this;
     }
-
+    @Step("Set Number")
     public SimpleFormWithPageObjects setUserNumber(String value) {
         userNumberInput.setValue(value);
         return this;
     }
-
+    @Step("Set BDay")
     public SimpleFormWithPageObjects setBirthDay(String day,
                                                  String month,
                                                  String year) {
@@ -70,38 +76,38 @@ public class SimpleFormWithPageObjects {
         calendarComponent.setDate(day, month, year);
         return this;
     }
-
+    @Step("Set Address")
     public SimpleFormWithPageObjects setCurrentAddress(String value) {
         currentAddressInput.setValue(value);
         return this;
     }
-
+    @Step("Set Hobby")
     public SimpleFormWithPageObjects setHobby(String sport) {
         hobbiesWrapper.$(byText(sport)).click();
         return this;
     }
-
+    @Step("Set Subjects")
     public SimpleFormWithPageObjects setSubjects(String value) {
         subjectsInput.setValue(value);
         subjectsInput.pressEnter();
         return this;
     }
-
+    @Step("Set City")
     public SimpleFormWithPageObjects setCity(String area, String city) {
         cityComponent.setCity(area, city);
         return this;
     }
-
+    @Step("Set Picture")
     public SimpleFormWithPageObjects uploadPicture(String value) {
         pictureFile.uploadFile(new File(value));
         return this;
     }
-
+    @Step("Click Submit Button")
     public SimpleFormWithPageObjects clickSubmit() {
         submitButton.click();
         return this;
     }
-
+    @Step("Verify results")
     public SimpleFormWithPageObjects checkResult(String fromTable, String fromUser) {
         verifyResultWO.verifyResults(fromTable, fromUser);
         return this;
--- src/test/java/com/hw13/tests/TestRemoteSimpleFormWithPageObjects.java
+++ src/test/java/com/hw13/tests/TestRemoteSimpleFormWithPageObjects.java
@@ -1,50 +1,49 @@
 package com.hw13.tests;
 import com.hw13.pages.SimpleFormWithPageObjects;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static com.hw13.utils.RandomUtils.*;
+import static io.qameta.allure.Allure.step;
+
 
 public class TestRemoteSimpleFormWithPageObjects extends TestRemoteBase {
     SimpleFormWithPageObjects simpleFormWithPageObjects = new SimpleFormWithPageObjects();
-    @Tag("formtest")
-    @Tag("remote")
+
     @Tag("properties")
     @Test
     @DisplayName("Checking form and result content with Objects")
     void successfulRegistrationTest() {
 
-        //обьявляем переменные и заносим в них рандомную дату из RundomUtils
-        String  randomFirstName = faker.name().firstName(),
-                randomLastN = faker.name().lastName(),
-                randomUserEmail = faker.internet().emailAddress(),
-                randomPhone = getRandomPhone(),
-                randomCurrentAddress = faker.address().fullAddress(),
-                randomUserGander = getRandomGender(),
-                randomHobby = getRandomHobby(),
-                randomSubjects = getRandomSubjects(),
-                randomDay = getRandomDay(),
-                randomMonth = getRandomMonth(),
-                randomYear = getRandomYear(),
-                randomState = getRandomState(),
-                randomCity = getRandomCity(randomState);
+             String randomFirstName = faker.name().firstName(),
+                    randomLastN = faker.name().lastName(),
+                    randomUserEmail = faker.internet().emailAddress(),
+                    randomPhone = getRandomPhone(),
+                    randomCurrentAddress = faker.address().fullAddress(),
+                    randomUserGander = getRandomGender(),
+                    randomHobby = getRandomHobby(),
+                    randomSubjects = getRandomSubjects(),
+                    randomDay = getRandomDay(),
+                    randomMonth = getRandomMonth(),
+                    randomYear = getRandomYear(),
+                    randomState = getRandomState(),
+                    randomCity = getRandomCity(randomState);
 
-        //Проверка заполения формы
-        simpleFormWithPageObjects.openPage()
-                .removeBanner()
-                .setFirstName(randomFirstName)
-                .setLastName(randomLastN)
-                .setUserEmailName(randomUserEmail)
-                .setGender(randomUserGander)
-                .setUserNumber(randomPhone)
-                .setBirthDay(randomDay, randomMonth, randomYear)
-                .setCurrentAddress(randomCurrentAddress)
-                .setHobby(randomHobby)
-                .setSubjects(randomSubjects)
-                .setCity(randomState, randomCity)
-                .uploadPicture("src/test/resources/1.png")
-                .clickSubmit();
-
+                    simpleFormWithPageObjects.openPage()
+                            .removeBanner()
+                            .setFirstName(randomFirstName)
+                            .setLastName(randomLastN)
+                            .setUserEmailName(randomUserEmail)
+                            .setGender(randomUserGander)
+                            .setUserNumber(randomPhone)
+                            .setBirthDay(randomDay, randomMonth, randomYear)
+                            .setCurrentAddress(randomCurrentAddress)
+                            .setHobby(randomHobby)
+                            .setSubjects(randomSubjects)
+                            .setCity(randomState, randomCity)
+                            .uploadPicture("src/test/resources/1.png")
+                            .clickSubmit();
         //Проверка данных попапа
         simpleFormWithPageObjects.checkResult("Student Name", randomFirstName)
                 .checkResult("Student Email", randomUserEmail)
